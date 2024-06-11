9266873c202f80c0a3a96fec718d8035ebb97596
Alex Sanchez <aa.sanchez.dev@gmail.com>
2024-06-11 17:57:51 -0400

    completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+const formData = {
+        name: "Chopper",
+        email: "theDevelopingDog@paws.com"
+    }
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+    
+function submitData(){
+   return fetch("http://localhost:3000/users", configurationObject)
+    .then((resp)=> {return resp.json()})
+    .then((obj)=> { const bodyElement = document.getElementsByTagName("body")[0];
+        const dataTextNode = document.createTextNode(obj.id);
+        bodyElement.appendChild(dataTextNode);        
+        })
+    .catch(function (error) {
+       alert("Bad things! Ragnarők!");
+        const bodyElement= document.getElementsByTagName("body")[0];
+        const dataTextNode= document.createTextNode(error.message);
+        bodyElement.appendChild(dataTextNode);
+
+      });
+}
+
+//document.querySelector("6e1d").appendChild(dogs)
